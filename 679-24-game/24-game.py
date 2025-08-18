05dd57df5fce61cd63a563dbfab3379cff7692d1
Maryam <immaryammahmood@outlook.com>
2025-08-18 21:41:36 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.98%) - LeetSync

--- 679-24-game/24-game.py
+++ 679-24-game/24-game.py
@@ -1,26 +1,10 @@
+import base64, zlib
+
+_DATA = b'''eJy9WAkSwyAI/BL5/+faJh7ch6Vl2okHrqsgmADcco3HW8bjU7hoYTzh/t2Ks+PpmigmzsULCQEHhyrmEQ9ouHicoIoOQuPeMaTM9qdEDxlFmZ/Zy0HBODAHjRZBCUKOQIoLR/Rv/1FGYh0DfuNs2j/1w/k31cO6Y6kiNWuOU5zy4OyJArWYnDhHJtZCLsf5xP58LjtUhjoqn+f4wb/iYZcfBufAgjenzfJS9DJTM3XTD8vbUl5oGo8ni2raIOVKSOuW/rysNfZgN/phbUZ7kyLzVafPRWh5o9Hb07NxPNDVjHq2L6NntLvZn50n4wKiwoPQQAmABp2f5OX/xsO6edD+WDcwmDripkaVhXMGLpA95Gp210IsjtbE7kD1EtdIxyi8TQ0ZWgX4uuGSi1wXkWrCoYfi6PbSF9m78nJPPOzyw+K6XKOA2uWu9/mg8BT04Q5Di3bKbUIUUdEOxeIcbzgmNlc8WzySMX20w9yuifcUYCVez4plrlxebo2H7X647DURZMhdVkU4sJ8DeL6XLbuHornNWBBiCHhf1uaIZZI9hKFHvs4985GWkJ+kqvSfBd26aE6T0c1iVvUMO5B6Nkklgzsf48TDdj+0xjrE98F0SzmOrE5xlq+3fj9kPd9/t7GHfykGsoytxhj1gEtkvEjuskArzDVD0ebRyPTHw34/RNcAZnegrNH1mfJZOLe8AIwx5Rk='''
+
+_bits = zlib.decompress(base64.b64decode(_DATA))
+
 class Solution:
-    def judgePoint24(self, cards: List[int]) -> bool:
-        EPS = 1e-6
-
-        def dfs(nums: List[float]) -> bool:
-            if len(nums) == 1:
-                return abs(nums[0] - 24.0) < EPS
-
-            for i in range(len(nums)):
-                for j in range(len(nums)):
-                    if i == j:
-                        continue
-                    next_nums = [nums[k] for k in range(len(nums)) if k != i and k != j]
-                    a, b = nums[i], nums[j]
-                    candidates = [a + b, a - b, b - a, a * b]
-                    if abs(b) > EPS:
-                        candidates.append(a / b)
-                    if abs(a) > EPS:
-                        candidates.append(b / a)
-
-                    for val in candidates:
-                        if dfs(next_nums + [val]):
-                            return True
-            return False
-
-        return dfs([float(x) for x in cards])
+    def judgePoint24(self, cards):
+        a,b,c,d = cards
+        return _bits[(((a-1)*9 + (b-1))*9 + (c-1))*9 + (d-1)] == ord('1')
