f83dfc3f36eddf3868d5fb13c73128c3cad36daa
Maryam <immaryammahmood@outlook.com>
2025-10-24 21:08:38 +0500

    Time: 5 ms (54.68%) | Memory: 25.6 MB (47.73%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        for (int i = 0; i < nums.size() - 2; i++)
-            if (nums[i + 1] + nums[i + 2] > nums[i])
-                return nums[i] + nums[i + 1] + nums[i + 2];
-        return 0;
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+
+        for(int i = 2; i < nums.size(); i++){
+            if(nums[i] < nums[i - 1] + nums[i - 2]) 
+                ans = max(ans, nums[i] + nums[i - 1] + nums[i - 2]);
+        }
+
+        return ans;
     }
 };
